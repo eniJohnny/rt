9eca6fc9488c5a4b1f15777aefd9974328a5263c
RuBeeN-cmd <rollinruben@live.fr>
2024-04-09 18:09:58 +0200

    Add unit test to vec2

--- src/model/maths/vec2.rs
+++ src/model/maths/vec2.rs
@@ -1,3 +1,8 @@
+use std::ops::{Add, Mul, Sub};
+use std::cmp::PartialEq;
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug)]
 pub struct Vec2 {
     x: f64,
     y: f64
@@ -30,5 +35,112 @@ impl Vec2 {
 			x: self.x / norm,
 			y: self.y / norm
 		}
+	}
+}
+
+impl Add for Vec2 {
+	type Output = Self;
+	fn add(self, rhs: Self) -> Self::Output {
+		Self {
+			x: self.x + &rhs.x,
+			y: self.y + &rhs.y
+		}
+	}
+}
+
+impl Sub for Vec2 {
+	type Output = Self;
+	fn sub(self, rhs: Self) -> Self::Output {
+		Self {
+			x: self.x - &rhs.x,
+			y: self.y - &rhs.y
+		}
+	}
+}
+
+impl Mul<Self> for Vec2 {
+	type Output = Self;
+	fn mul(self, rhs: Self) -> Self::Output {
+		Self {
+			x: self.x * rhs.x,
+			y: self.y * rhs.y
+		}
+	}
+}
+
+impl Mul<f64> for Vec2 {
+	type Output = Self;
+	fn mul(self, rhs: f64) -> Self::Output {
+		Self {
+			x: self.x * rhs,
+			y: self.y * rhs
+		}
+	}
+}
+
+impl PartialEq for Vec2 {
+	fn eq(&self, other: &Self) -> bool {
+		self.x == other.x && self.y == other.y
+	}
+
+	fn ne(&self, other: &Self) -> bool {
+		self.x != other.x || self.y != other.y
+	}
+}
+
+impl Display for Vec2 {
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		write!(f, "({}, {})", self.x, self.y)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::model::maths::vec2::Vec2;
+
+	#[test]
+	fn test_add() {
+		let v1: Vec2 = Vec2::new(1., 2.);
+		let v2: Vec2 = Vec2::new(3., 4.);
+		assert_eq!(v1 + v2, Vec2::new(4., 6.));
+	}
+
+	#[test]
+	fn test_sub() {
+		let v1: Vec2 = Vec2::new(1., 2.);
+		let v2: Vec2 = Vec2::new(3., 4.);
+		assert_eq!(v1 - v2, Vec2::new(-2., -2.));
+	}
+
+	#[test]
+	fn test_mul() {
+		let v1: Vec2 = Vec2::new(1., 2.);
+		let v2: Vec2 = Vec2::new(3., 4.);
+		assert_eq!(v1 * v2, Vec2::new(3., 8.));
+	}
+
+	#[test]
+	fn test_mul_scalar() {
+		let v1: Vec2 = Vec2::new(1., 2.);
+		assert_eq!(v1 * 2., Vec2::new(2., 4.));
+	}
+
+	#[test]
+	fn test_dot() {
+		let v1: Vec2 = Vec2::new(1., 2.);
+		let v2: Vec2 = Vec2::new(3., 4.);
+		assert_eq!(v1.dot(&v2), 11.);
+	}
+
+	#[test]
+	fn test_get_norm() {
+		let v1: Vec2 = Vec2::new(3., 4.);
+		assert_eq!(v1.get_norm(), 5.);
+	}
+
+	#[test]
+	fn test_normalize() {
+		let v1: Vec2 = Vec2::new(3., 4.);
+		assert_eq!(v1.normalize().get_norm(), 1.);
 	}
 }
