e40bd98603b93e756f164ae9e0e9eb0e6a8f6b17
RuBeeN-cmd <rollinruben@live.fr>
2024-04-09 16:47:18 +0200

    Add getters, norm, dot to vec2

--- src/model/maths/vec2.rs
+++ src/model/maths/vec2.rs
@@ -2,3 +2,33 @@ pub struct Vec2 {
     x: f64,
     y: f64
 }
+
+impl Vec2 {
+	pub fn new(x: f64, y: f64) -> Self {
+		Self { x, y }
+	}
+
+	pub fn x(&self) -> &f64 {
+		&self.x
+	}
+
+	pub fn y(&self) -> &f64 {
+		&self.y
+	}
+
+	pub fn dot(&self, other: &Self) -> f64 {
+		self.x * other.x + self.y * other.y
+	}
+
+	pub fn get_norm(&self) -> f64 {
+		(self.x * self.x + self.y * self.y).sqrt()
+	}
+
+	pub fn normalize(&self) -> Self {
+		let norm: f64 = self.get_norm();
+		Self {
+			x: self.x / norm,
+			y: self.y / norm
+		}
+	}
+}
