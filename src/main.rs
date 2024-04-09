6bfa27e970124d6558a9a6a6bfb9105040a193a4
Kamiakze69j <kamiakze69j@gmail.com>
2024-04-09 17:02:05 +0200

    modified:   scenes/scene.json 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ fn main() {
 
 fn parse_json() {
     let content = std::fs::read_to_string("scenes/scene.json").unwrap();
-    let mut objects: Vec<HashMap<String, &str>> = Vec::new();
+    let mut objects: Vec<HashMap<String, String>> = Vec::new();
     let mut i = 0;
 
     println!("\n---Start---\n");
     while i < content.len() && content[i..].find('{') != None {
-        let mut object: HashMap<String, &str> = HashMap::new();
+        let mut object: HashMap<String, String> = HashMap::new();
         let remaining = &content[i..];
         let start = remaining.find('{').unwrap();
         let end = remaining.find("\n    }").unwrap() + 6;
@@ -31,15 +31,37 @@ fn parse_json() {
         for prop in object_str.split(",\n        \"") {
             let prop = prop.trim();
             let mut prop = prop.split(": ");
-            let key = prop.next().unwrap().trim_matches(['"', ' ', '\n', '{', '}']).to_string();
-            let value = prop.next().unwrap().trim_matches(['{', '"', ' ', '\n', '}']);
+            let key: String = prop.next().unwrap().trim_matches(['"', ' ', '\n', '{', '}']).to_string();
+            let value: String = prop.next().unwrap().trim_matches(['{', '"', ' ', '\n', '}']).to_string();
 
-            object.insert(key, value);
+            if value.contains('[') {
+                let str = value.trim_matches(['[', ']']).replace(", ", ",");
+                let tmp: Vec<&str> = str.split(",").collect();
+
+                if key == "position" {
+                    object.insert("position_x".to_string(), tmp[0].to_string());
+                    object.insert("position_y".to_string(), tmp[1].to_string());
+                    object.insert("position_z".to_string(), tmp[2].to_string());
+                } else if key == "direction" {
+                    object.insert("direction_x".to_string(), tmp[0].to_string());
+                    object.insert("direction_y".to_string(), tmp[1].to_string());
+                    object.insert("direction_z".to_string(), tmp[2].to_string());
+                } else if key == "color" {
+                    object.insert("color_r".to_string(), tmp[0].to_string());
+                    object.insert("color_g".to_string(), tmp[1].to_string());
+                    object.insert("color_b".to_string(), tmp[2].to_string());
+                }
+
+            } else {
+                object.insert(key, value);
+            }
         }
         objects.push(object);
     }
     // Here, objects is a vector of HashMaps, each representing an object in the scene.
-    println!("{:?}", objects);
+    for object in objects {
+        println!("{:?}", object);
+    }
 }
 
 fn view(app: &App, frame: Frame) {
