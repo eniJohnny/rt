846d607556077b6f0adb56b15a6009fe7eeacdb5
johrober <johrober@student.42angouleme.fr>
2024-04-10 16:12:03 +0200

    Continuation de la main pipeline et amelioration du modele

--- src/model/maths/hit.rs
+++ src/model/maths/hit.rs
@@ -5,6 +5,35 @@ use super::vec3::Vec3;
 pub struct Hit<'a> {
     element: &'a Element,
     dist: f64,
-    norm: Vec3,
-    pos: Vec3
+    pos: Vec3,
+    norm: Option<Vec3>,
+    projected_pos: Option<(i32, i32)>
+}
+
+impl<'a> Hit<'a> {
+    pub fn new(element: &'a Element, dist: f64, pos: Vec3) -> Self {
+        Hit {
+            element, 
+            dist,
+            pos,
+            norm :None,
+            projected_pos: None
+        }
+    }
+
+    pub fn element(&self) -> &'a Element {
+        self.element
+    }
+
+    pub fn dist(&self) -> &f64 {
+        &self.dist
+    }
+
+    pub fn norm(&self) -> &Vec3 {
+        match &self.norm {
+            Some(vec) => vec,
+            None => unimplemented!()
+        }
+    }
+
 }
