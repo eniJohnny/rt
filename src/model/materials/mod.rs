7b2afbf2d4793cbbee2cbb6e4613ad454e664dfb
pmailly <98979520+pmaily@users.noreply.github.com>
2024-04-12 14:19:07 +0200

    GUI, Display, Shapes, Parsing (#14)

--- src/model/materials/mod.rs
+++ src/model/materials/mod.rs
@@ -1,27 +1,46 @@
 use super::maths::vec3::Vec3;
+use crate::model::materials::unicolor::Unicolor;
+use std::fmt::Debug;
+pub mod unicolor;
 
-mod unicolor;
-
+#[derive(Clone, Debug)]
 pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8
+    r: u8,
+    g: u8,
+    b: u8
 }
 
 impl Color {
-    pub fn new(color: &Self) -> Self{
-        Self {
-            r: color.r,
-            g: color.g,
-            b: color.b
-        }
+    // Getters
+    pub fn r(&self) -> u8 {
+        self.r
+    }
+    pub fn g(&self) -> u8 {
+        self.g
+    }
+    pub fn b(&self) -> u8 {
+        self.b
+    }
+
+    // Constructors
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
     }
 }
 
-pub trait Material {
+pub trait Material: Debug {
     fn color(&self, x: i32, y: i32) -> Color;
     fn norm(&self, x: i32, y: i32) -> Vec3;
     fn reflection_coef(&self) -> f64;
     fn refraction_coef(&self) -> f64;
     fn needs_projection(&self) -> bool;
+}
+
+impl dyn Material {
+    pub fn new(color: Color) -> Box<Self> {
+        Box::new(Unicolor::new(color.r(), color.g(), color.b()))
+    }
+    pub fn default() -> Box<Self> {
+        Box::new(Unicolor::new(0, 0, 0))
+    }
 }
--- src/model/objects/light.rs
+++ src/model/objects/light.rs
@@ -1,6 +1,48 @@
-use crate::model::maths::vec3::Vec3;
+use crate::model::{materials::Color, maths::vec3::Vec3};
 
-struct Light {
+#[derive(Debug)]
+pub struct Light {
     pos: Vec3,
-    dir: Vec3
+    intensity: f64,
+    color: Color
+}
+
+impl Light {
+    // Accessors
+    pub fn pos(&self) -> &Vec3 { &self.pos }
+    pub fn intensity(&self) -> f64 { self.intensity }
+    pub fn color(&self) -> &Color { &self.color }
+
+    // Mutators
+    pub fn set_pos(&mut self, pos: Vec3) { self.pos = pos }
+    pub fn set_intensity(&mut self, intensity: f64) { self.intensity = intensity }
+    pub fn set_color(&mut self, color: Color) { self.color = color }
+
+    // Constructor
+    pub fn new(pos: Vec3, intensity: f64, color: Color) -> Self {
+        self::Light { pos, intensity, color }
+    }
+}
+
+#[derive(Debug)]
+pub struct AmbientLight {
+    intensity: f64,
+    color: Color
+}
+
+impl AmbientLight {
+    // Accessors
+    pub fn intensity(&self) -> f64 { self.intensity }
+    pub fn color(&self) -> &Color { &self.color }
+
+    // Constructor
+    pub fn new(intensity: f64, color: Color) -> Self {
+        self::AmbientLight { intensity, color }
+    }
+    pub fn default() -> Self {
+        Self {
+            intensity: 0.5,
+            color: Color::new(255, 255, 255)
+        }
+    }
 }
