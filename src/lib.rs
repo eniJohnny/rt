c14030646c964134cc91556f1ae44ec4e6008a7f
pmailly <98979520+pmaily@users.noreply.github.com>
2024-04-12 14:19:07 +0200

    GUI, Display, Shapes, Parsing (#14)

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,28 @@
 use model::scene::Scene;
-use parsing::get_scene;
-use render::render_scene;
+use parsing::{get_scene, print_scene};
+use display::display_scene;
 
 pub mod gui;
 pub mod model;
 pub mod parsing;
 pub mod render;
+pub mod events;
+pub mod display;
 
-const WINDOW_WIDTH: usize = 1900;
-const WINDOW_HEIGHT: usize = 1080;
+const SCREEN_WIDTH: u32 = 1600;
+const SCREEN_HEIGHT: u32 = 900;
 
 pub fn run() {
     let scene = get_scene();
+    
+    print_scene(&scene);
+    event_loop(&scene);
 }
 
 pub fn event_loop(scene: &Scene) {
-    render_scene(scene)
+    display_scene(scene)
+}
+
+pub fn error(msg: &str) {
+    eprintln!("Error: {}", msg);
 }
--- src/model/scene.rs
+++ src/model/scene.rs
@@ -1,24 +1,75 @@
+use crate::model::objects::light::{AmbientLight, Light};
+
 use super::{objects::camera::Camera, Element};
 
-
+#[derive(Debug)]
 pub struct Scene {
+    elements: Vec<Element>,
     camera: Camera,
-    elements: Vec<Element>
+    lights: Vec<Light>,
+    ambient_light: AmbientLight
 }
 
 impl Scene {
-    pub fn new(camera: Camera, elements: Vec<Element>) -> Self {
-        Self{
-            camera,
-            elements
+    pub fn new() -> Self {
+        Self {
+            elements: Vec::new(),
+            camera: Camera::default(),
+            lights: Vec::new(),
+            ambient_light: AmbientLight::default() 
         }
     }
 
-    pub fn camera(&self) -> &Camera {
-       &self.camera
+    // Adders
+    pub fn add_element(&mut self, element: Element) {
+        self.elements.push(element);
     }
 
+    pub fn add_camera(&mut self, camera: Camera) {
+        self.camera = camera;
+    }
+
+    pub fn add_light(&mut self, light: Light) {
+        self.lights.push(light);
+    }
+
+    pub fn add_ambient_light(&mut self, ambient_light: AmbientLight) {
+        self.ambient_light = ambient_light;
+    }
+
+    // Accessors
     pub fn elements(&self) -> &Vec<Element> {
         &self.elements
     }
+
+    pub fn camera(&self) -> &Camera {
+        &self.camera
+    }
+
+    pub fn lights(&self) -> &Vec<Light> {
+        &self.lights
+    }
+
+    pub fn ambient_light(&self) -> &AmbientLight {
+        &self.ambient_light
+    }
+
+    // Mutators
+
+    pub fn set_elements(&mut self, elements: Vec<Element>) {
+        self.elements = elements;
+    }
+
+    pub fn set_camera(&mut self, camera: Camera) {
+        self.camera = camera;
+    }
+
+    pub fn set_lights(&mut self, lights: Vec<Light>) {
+        self.lights = lights;
+    }
+
+    pub fn set_ambient_light(&mut self, ambient_light: AmbientLight) {
+        self.ambient_light = ambient_light;
+    }
+
 }
