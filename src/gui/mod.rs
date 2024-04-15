b8f18804fe4d9a2af4fbcabc9edef44b6edeabe1
kamiakze69j <60266310+kamiakze69j@users.noreply.github.com>
2024-04-15 15:20:16 +0200

    Graphics library change (#21)
    * display vector of vector
    
    * sped up display
    
    * 8 implementation du raycasting (#20)
    
    * Algorithme de generation fait et non tester(Requiert changement a vec3)
    
    * Fixed conflicts
    
    * Imbriquement des differentes pieces apres rebase
    
    * Fix de la generation des rays + Intersections
    
    ---------
    
    Co-authored-by: johrober <johrober@student.42angouleme.fr>
    
    * Changed graphics library from nannou to pixels, image and winit.
    Merged Johrober's code
    Benchmark printing
    
    * small fixes
    
    ---------
    
    Co-authored-by: johrober <johnny.robert208@gmail.com>
    Co-authored-by: johrober <johrober@student.42angouleme.fr>

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,76 +1,40 @@
-// use nannou::{draw, prelude::*};
-// use crate::{SCREEN_WIDTH, SCREEN_HEIGHT};
+use image::Rgba;
 
-// pub(crate) fn draw_gui(draw: &draw::Draw) {
+use crate::GUI_WIDTH;
 
-//     let gui_x_start: f32 = (SCREEN_WIDTH / 2 - 200) as f32;
-//     let gui_x_end: f32 = (SCREEN_WIDTH  / 2) as f32;
-//     let gui_y_start: f32 = (SCREEN_HEIGHT / 2) as f32;
-//     let gui_y_end: f32 = (SCREEN_HEIGHT as i32 / 2 * -1) as f32;
+pub fn draw_gui (img: image::ImageBuffer<Rgba<u8>, Vec<u8>>) -> image::ImageBuffer<Rgba<u8>, Vec<u8>>{
+    // Create a new RGBA image
+    let width = img.width();
+    let x_start = width - GUI_WIDTH;
+    let height = img.height();
+    let mut img = img;
 
-//     line_put(Point2::new(gui_x_start, gui_y_start), Point2::new(gui_x_start, gui_y_end), nannou::color::WHITE, &draw);
+    // Draw the GUI
+    for x in x_start..width {
+        for y in 0..height {
+            if is_border(x, y, x_start, width, height) {
+                img.put_pixel(x, y, Rgba([230, 230, 230, 255]));
+            } else {
+                img.put_pixel(x, y, Rgba([255, 255, 255, 255]));
+            }
+        }
+    }
 
-//     let test_button: [Point2; 2] = [
-//         Point2::new(gui_x_start + 20., gui_y_start - 20.),
-//         Point2::new(gui_x_end - 20., gui_y_start - 70.)
-//     ];
+    img
+}
 
-//     rect_put(test_button[0], test_button[1], nannou::color::GRAY, &draw);
-// }
+fn is_border (x: u32, y: u32, x_start: u32, width: u32, height: u32) -> bool {
+    let border_width = 4;
 
-// fn pixel_put(x: i32, y: i32, color: nannou::color::Rgb<u8>, draw: &draw::Draw) {
-//     draw.rect()
-//         .w_h(1.0, 1.0)
-//         .x_y(x as f32, y as f32)
-//         .color(color);
-// }
-
-// #[allow(dead_code)]
-// fn line_put(start: Point2, end: Point2, color: nannou::color::Rgb<u8>, draw: &draw::Draw) {
-
-//     let x = end.x - start.x;
-//     let y = end.y - start.y;
-//     let max = if abs(x) > abs(y) { abs(x) } else { abs(y) };
-//     let x_step = x / max;
-//     let y_step = y / max;
-//     let mut current = start;
-//     for _ in 0..max as i32 {
-//         pixel_put(current.x as i32, current.y as i32, color, draw);
-//         current.x += x_step;
-//         current.y += y_step;
-//     }
-// }
-
-// #[allow(dead_code)]
-// fn rect_put(start: Point2, end: Point2, color: nannou::color::Rgb<u8>, draw: &draw::Draw) {
-//     let mut upper_left = Point2::default();
-//     let mut lower_right = Point2::default();
-
-//     upper_left.x = if start.x < end.x { start.x } else { end.x };
-//     upper_left.y = if start.y < end.y { start.y } else { end.y };
-//     lower_right.x = if start.x > end.x { start.x } else { end.x };
-//     lower_right.y = if start.y > end.y { start.y } else { end.y };
-
-//     let upper_right = Point2::new(lower_right.x, upper_left.y);
-//     let lower_left = Point2::new(upper_left.x, lower_right.y);
-
-//     line_put(upper_left, upper_right, color, draw);
-//     line_put(upper_right, lower_right, color, draw);
-//     line_put(lower_right, lower_left, color, draw);
-//     line_put(lower_left, upper_left, color, draw);
-// }
-
-// #[allow(dead_code)]
-// fn filled_rect_put(start: Point2, end: Point2, color: nannou::color::Rgb<u8>, draw: &draw::Draw) {
-//     let mut upper_left = Point2::default();
-//     let mut lower_right = Point2::default();
-
-//     upper_left.x = if start.x < end.x { start.x } else { end.x };
-//     upper_left.y = if start.y < end.y { start.y } else { end.y };
-//     lower_right.x = if start.x > end.x { start.x } else { end.x };
-//     lower_right.y = if start.y > end.y { start.y } else { end.y };
-
-//     for y in upper_left.y as i32..lower_right.y as i32 {
-//         line_put(Point2::new(upper_left.x, y as f32), Point2::new(lower_right.x, y as f32), color, draw);
-//     }
-// }
+    if x < x_start + border_width {
+        return true;
+    } else if x >= width - border_width {
+        return true;
+    } else if y < border_width {
+        return true;
+    } else if y > height - border_width {
+        return true;
+    } else {
+        return false;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,17 @@
 extern crate image;
-extern crate minifb;
-
-use std::f64::consts::PI;
 use display::display_scene;
 use model::scene::Scene;
 use parsing::get_scene;
-
 
 pub mod gui;
 pub mod model;
 pub mod parsing;
 pub mod render;
-pub mod events;
 pub mod display;
 
-const SCREEN_WIDTH: usize = 1600;
-const SCREEN_HEIGHT: usize = 900;
-
-const VFOV: i32 = 90;
-const VFOV_RAD: f64 = VFOV as f64 * 2. * PI / 360.;
+const SCREEN_WIDTH: u32 = 1600;
+const SCREEN_HEIGHT: u32 = 900;
+const GUI_WIDTH: u32 = 200;
 
 pub fn run() {
     let scene = get_scene();
@@ -28,7 +21,7 @@ pub fn run() {
 }
 
 pub fn event_loop(scene: &Scene) {
-    display_scene(scene)
+    display_scene(scene);
 }
 
 pub fn error(msg: &str) {
