80d4efba8f13ac023b50c807cdcd9844304f7e94
pmailly <paulma16100@gmail.com>
2024-04-16 15:52:09 +0200

    Intersection plan

--- src/model/shapes/plane.rs
+++ src/model/shapes/plane.rs
@@ -11,8 +11,19 @@ impl Shape for Plane {
     fn distance(&self, vec: &Vec3) -> f64 {
         unimplemented!()
     }
-    fn intersect(&self, vector: &Ray) -> Option<Vec<f64>> {
-        unimplemented!()
+    fn intersect(&self, r: &Ray) -> Option<Vec<f64>> {
+
+        let dist = &self.pos - r.get_pos();
+        let normalized_dir = r.get_dir().dot(&self.dir);
+        if (normalized_dir < 0.0) {
+            return None;
+        }
+        let test = dist.dot(&self.dir);
+        let t = test / normalized_dir;
+        if (t > 0.0) {
+            return Some(Vec::from([t]));
+        }
+        return None;
     }
     fn projection(&self, hit: &Hit) -> (i32, i32) {
         unimplemented!()
@@ -36,4 +47,18 @@ impl Plane {
         self::Plane { pos, dir }
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::model::maths::ray::Ray;
+    use crate::model::maths::vec3::Vec3;
+    use crate::model::shapes::Shape;
+
+    #[test]
+    fn test_plane_intersect() {
+        let plane = super::Plane::new(Vec3::new(0., 0., 1.), Vec3::new(0., 0., 1.));
+        let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0), 5);
+        assert_eq!(plane.intersect(&ray), Some(Vec::from([1.0])));
+    }
 }
