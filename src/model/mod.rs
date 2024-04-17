1fc8b432c28592606167758e9c76250c6d5986f5
pmailly <98979520+pmaily@users.noreply.github.com>
2024-04-17 18:43:14 +0200

    Intersections (#28)
    * Intersection plan
    
    * Modification map

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,5 +1,3 @@
-use std::any::Any;
-
 use self::{materials::Material, shapes::Shape};
 
 pub mod materials;
--- src/model/shapes/plane.rs
+++ src/model/shapes/plane.rs
@@ -11,8 +11,18 @@ impl Shape for Plane {
     fn distance(&self, vec: &Vec3) -> f64 {
         unimplemented!()
     }
-    fn intersect(&self, vector: &Ray) -> Option<Vec<f64>> {
-        unimplemented!()
+    fn intersect(&self, r: &Ray) -> Option<Vec<f64>> {
+
+        let dist = &self.pos - r.get_pos();
+        let dot_product = r.get_dir().dot(&self.dir);
+        if (dot_product >0. ) {
+            return None;
+        }
+        let t = dist.dot(&self.dir) / dot_product;
+        if (t > 0.0) {
+            return Some(Vec::from([t]));
+        }
+        return None;
     }
     fn projection(&self, hit: &Hit) -> (i32, i32) {
         unimplemented!()
@@ -36,4 +46,25 @@ impl Plane {
         self::Plane { pos, dir }
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::model::maths::ray::Ray;
+    use crate::model::maths::vec3::Vec3;
+    use crate::model::shapes::Shape;
+
+    #[test]
+    fn test_plane_intersect1() {
+        let plane = super::Plane::new(Vec3::new(0., 0., 1.), Vec3::new(0., 0., 1.));
+        let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0), 5);
+        assert_eq!(plane.intersect(&ray), None);
+    }
+
+    #[test]
+    fn test_plane_intersect2() {
+        let plane = super::Plane::new(Vec3::new(0., 0., 1.), Vec3::new(0., 0., -1.));
+        let ray = Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0), 5);
+        assert_eq!(plane.intersect(&ray), Some(Vec::from([1.0])));
+    }
 }
