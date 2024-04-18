dfd48f57f926362b6c764df9b51228f6750d0c90
RuBeeN-cmd <rollinruben@live.fr>
2024-04-18 14:15:51 +0200

    Dont know how to write "PARRALLEL"

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -98,7 +98,7 @@ pub fn get_scene() -> Scene {
                 let dir = get_direction(&object);
                 let color = get_color(&object);
 
-                let new_light = Box::new(ParrallelLight::new(dir, intensity, color)) as Box<dyn Light>;
+                let new_light = Box::new(ParallelLight::new(dir, intensity, color)) as Box<dyn Light>;
                 scene.add_light(new_light);
             },
             _ => {}
