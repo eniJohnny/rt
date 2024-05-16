da30e348ff52cf815b0cefb824eaf65b8ac9a549
RuBeeN-cmd <rollinruben@live.fr>
2024-05-16 19:06:02 +0200

    Still working

--- src/render/lighting.rs
+++ src/render/lighting.rs
@@ -48,7 +48,6 @@ pub fn apply_lighting_bounce(hit: &Hit, scene: &Scene, ray: &Ray) -> Color {
 		light_color + scene.ambient_light().intensity() * scene.ambient_light().color() * &color;
 	for light in scene.lights() {
 		light_color = light_color + light.as_ref().get_diffuse(&hit) * &color;
-		light_color = light_color + light.as_ref().get_specular(&hit, ray);
 	}
 	(light_color).clamp(0., 1.)
 }
@@ -56,19 +55,7 @@ pub fn apply_lighting_bounce(hit: &Hit, scene: &Scene, ray: &Ray) -> Color {
 pub fn get_bounce_color(scene: &Scene, ray: &Ray) -> Color {
     match get_closest_hit(scene, ray) {
         Some(hit) => {
-            let material = hit.element().material();
-            let absorbed = 1.0 - material.reflection_coef() - material.refraction_coef();
-            let mut color = apply_lighting_bounce(&hit, scene, ray) * absorbed;
-            if hit.element().material().reflection_coef() > f64::EPSILON {
-                if ray.get_depth() < MAX_DEPTH {
-                    let dir = (ray.get_dir() - 2. * ray.get_dir().dot(hit.norm()) * hit.norm())
-                        .normalize();
-                    let ray = Ray::new(hit.pos().clone() + &dir * 0.01, dir, ray.get_depth() + 1);
-                    color = color + get_bounce_color(scene, &ray) * material.reflection_coef();
-                }
-            }
-            color.apply_gamma();
-            color
+            apply_lighting_bounce(&hit, scene, ray)
         }
         None => Color::new(0., 0., 0.),
     }
@@ -83,27 +70,56 @@ pub fn random_bounce(ray: &Ray, normal: &Vec3, roughness: f64) -> Ray {
 
 pub fn get_reflected_light(hit: &Hit, scene: &Scene, ray: &Ray) -> Color {
 	let mut bucket: Bucket = Bucket {
-		ray: Ray::new(ray.get_pos() + hit.norm() * 0.001,
+		ray: Ray::new(hit.pos() + hit.norm() * 0.001,
 					reflect_dir(ray.get_dir(), hit.norm()),
 					ray.get_depth() + 1),
 		weight: 0.,
 	};
 	let sample_nb = 1;
 	if hit.element().material().roughness() > f64::EPSILON {
-		for _ in 0..sample_nb {
+		for i in 0..sample_nb {
 			let random_bounce = random_bounce(&ray, hit.norm(), hit.element().material().roughness());
 			let bounce_color = get_bounce_color(scene, &random_bounce);
 			let weight = bounce_color.r() + bounce_color.g() + bounce_color.b();
-			let rand: f64 = rand::thread_rng().gen_range((0.)..(bucket.weight + weight));
 			bucket.weight += weight;
-			if rand > bucket.weight {
+			if i == 0 {
 				bucket.ray = random_bounce;
+			} else {
+				let rand: f64 = rand::thread_rng().gen_range((0.)..(bucket.weight + weight));
+				if rand > bucket.weight {
+					bucket.ray = random_bounce;
+				}	
 			}
 		}
 	}
 
 	cast_ray(scene, &bucket.ray)
 }
+
+
+// pub fn get_indirect_light(hit: &Hit, scene: &Scene, ray: &Ray) -> Color {
+// 	let mut bucket: Bucket = Bucket {
+// 		ray: Ray::new(ray.get_pos() + hit.norm() * 0.001,
+// 					ray.get_dir().clone(),
+// 					ray.get_depth() + 1),
+// 		weight: 0.,
+// 	};
+// 	let sample_nb = 1;
+// 	if hit.element().material().roughness() > f64::EPSILON {
+// 		for _ in 0..sample_nb {
+// 			let random_bounce = random_bounce(&ray, hit.norm(), hit.element().material().roughness());
+// 			let bounce_color = get_bounce_color(scene, &random_bounce);
+// 			let weight = bounce_color.r() + bounce_color.g() + bounce_color.b();
+// 			let rand: f64 = rand::thread_rng().gen_range((0.)..(bucket.weight + weight));
+// 			bucket.weight += weight;
+// 			if rand > bucket.weight {
+// 				bucket.ray = random_bounce;
+// 			}
+// 		}
+// 	}
+
+// 	cast_ray(scene, &bucket.ray)
+// }
 
 pub fn apply_lighting(hit: &Hit, scene: &Scene, ray: &Ray) -> Color {
 
@@ -126,10 +142,10 @@ pub fn apply_lighting(hit: &Hit, scene: &Scene, ray: &Ray) -> Color {
 	    }
 	}
 
-	// Indirect light
-	if ray.get_depth() < MAX_DEPTH {
-		light_color = light_color;
-	}
+	// // Indirect light
+	// if ray.get_depth() < MAX_DEPTH {
+	// 	light_color = light_color + get_indirect_light(&hit, scene, ray);
+	// }
 	
 	(light_color).clamp(0., 1.);
 
