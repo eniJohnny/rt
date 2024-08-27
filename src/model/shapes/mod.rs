759c6705c93f98b0b36122059aec1ecabaaf417f
pmailly <98979520+pmaily@users.noreply.github.com>
2024-08-27 12:23:38 +0200

    Intersection triangle + rectangle (#64)
    * triangle rectangle
    
    * end

--- src/model/shapes/mod.rs
+++ src/model/shapes/mod.rs
@@ -2,7 +2,10 @@ use self::cone::Cone;
 use self::cylinder::Cylinder;
 use self::plane::Plane;
 use self::sphere::Sphere;
+use crate::model::shapes::rectangle::Rectangle;
+
 use std::fmt::Debug;
+use crate::model::shapes::triangle::Triangle;
 
 use super::{
     materials::material::Projection,
@@ -13,6 +16,8 @@ pub mod cone;
 pub mod cylinder;
 pub mod plane;
 pub mod sphere;
+pub mod rectangle;
+pub mod triangle;
 
 pub trait Shape: Debug + Sync + Send {
     fn distance(&self, vec: &Vec3) -> f64;
@@ -31,6 +36,12 @@ pub trait Shape: Debug + Sync + Send {
         None
     }
     fn as_cone(&self) -> Option<&Cone> {
+        None
+    }
+    fn as_rectangle(&self) -> Option<&Rectangle> {
+        None
+    }
+    fn as_triangle(&self) -> Option<&Triangle> {
         None
     }
 }
