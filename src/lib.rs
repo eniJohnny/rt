3c3a2725b79e3f7d84b57794faecca83e2142f5d
johrober <johrober@42angoulemes.fr>
2024-12-10 17:39:21 +0100

    Switched scene to metalrough

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub mod bvh;
 
 const USING_BVH: bool = true;
 const SCENE_FOLDER: &str = "scenes";
-const SCENE: &str = "cube";
+const SCENE: &str = "metalrough";
 
 /************* Camera **************/
 const STEP: f64 = 0.2;
