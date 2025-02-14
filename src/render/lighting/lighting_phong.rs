102e1f9d49cb5c253b4ed24cc9e267153ff606b0
johrober <johnny.robert208@gmail.com>
2025-02-14 13:38:34 +0100

    Revert of the changes made by #94 + Proper fix + Reduction of the blinding effect in phong (#98)
    * Revert de la correction sur phong et fix du bug initial
    
    * Reduced the diffuse component of the blinding effect
    
    ---------
    
    Co-authored-by: johrober <johrober@42angoulemes.fr>

--- src/render/lighting/lighting_phong.rs
+++ src/render/lighting/lighting_phong.rs
@@ -14,32 +14,28 @@ pub fn phong_lighting_from_hit(scene: &Scene, hit: &Option<Hit>, ray: &Ray) -> C
 		let hit: Hit<'_> = Hit::new(&elem, 0., *scene.camera().pos(), fake_ray.get_dir(), scene.textures(), vec![0.]);
 		for light in scene.lights() {
 			let throughput = light.light().throughput(scene, &hit);
-			if throughput.length() > -f64::EPSILON {
+			if throughput.length() > f64::EPSILON {
 				if ray.debug {
 					fake_ray.debug = true;
 				}
 				let specular = light.light().get_specular(&hit, &fake_ray);
 				let diffuse = light.light().get_diffuse(&hit);
-				cam_color += diffuse + specular;
+				cam_color += &diffuse * &diffuse + specular;
 				cam_color = cam_color * Color::from_vec3(&throughput);
 			}
 		}
 	}
 	if let Some(hit) = hit {
-		let mut colors: Vec<Color> = vec![];
 		let mut color: Color = scene.ambient_light().color() * scene.ambient_light().intensity() * hit.color();
 	
 		for light in scene.lights() {
 			let throughput = light.light().throughput(scene, &hit);
-			if throughput.length() > -f64::EPSILON {
+			if throughput.length() > f64::EPSILON {
 				color += light.light().get_diffuse(&hit) * hit.color();
 				color += light.light().get_specular(&hit, ray);
 				color = color * Color::from_vec3(&throughput);
-				colors.push(color);
-				color = scene.ambient_light().color() * scene.ambient_light().intensity() * hit.color();
 			}
 		}
-		color = colors.iter().fold(Color::new(0., 0., 0.), |acc, x| acc + x);
 		if hit.opacity() < f64::EPSILON {
 			let light_through = get_lighting_from_ray(scene, &Ray::new(hit.pos().clone() + *ray.get_dir() * BOUNCE_OFFSET, ray.get_dir().clone(), ray.get_depth()));
 			color = color * hit.opacity() + hit.color() * light_through * (1. - hit.opacity());
