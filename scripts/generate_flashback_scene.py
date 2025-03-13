511d859c2591f4e8d516353c1052ed1ea416fd7c
johrober <johrober@42angoulemes.fr>
2025-03-13 17:35:45 +0100

    Tweaked the flashback scene generation script + Reversed the scroll direction + Upped the scroll strenght

---
+++ scripts/generate_flashback_scene.py
@@ -0,0 +1,53 @@
+import os
+
+def rename_screens():
+    path = 'textures/screenshots'
+    files = os.listdir(path)
+    files = [file for file in files if file.endswith('.png')]
+    
+    indexed_files = []
+    for file in files:
+        index = file.split('_')[0]
+        indexed_files.append((int(index), file))
+
+    indexed_files.sort(key=lambda x: x[0])
+    indexed_files.reverse()
+    
+    for i, file in enumerate(indexed_files):
+        os.rename(f'{path}/{file[1]}', f'{path}/{i + 1}.png')
+
+def generate_flashback():
+    n = len(os.listdir('textures/flashback'))
+    content = """[
+    {
+        "type": "skybox",
+        "color": "skybox/skybox_night.jpg"
+    },
+    {
+        "type": "camera",
+        "pos": [0, 0, 0],
+        "dir": [0, 0, 1],
+        "fov": 70
+    }"""
+
+    for i in range(n):
+        content += ',\n    {\n'
+        content += '        "type": "rectangle",\n'
+        content += f'        "pos": [4, 0, {i * 10 + 20}],\n'
+        content += '        "dir_w": [0, 0, -1],\n'
+        content += '        "dir_l": [0, 1, 0],\n'
+        content += '        "length": 4.5,\n'
+        content += '        "width": 8,\n'
+        content += f'        "color": "flashback/{i + 1}.png"\n'
+        content += '    }'
+
+    content += '\n]'
+
+
+    with open('scenes/flashback.json', 'w') as file:
+        file.seek(0)
+        file.write(content)
+
+if __name__ == '__main__':
+    # rename_screens()
+    generate_flashback()
